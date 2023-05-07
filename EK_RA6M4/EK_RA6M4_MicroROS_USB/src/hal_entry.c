349e7ca70ceb99737af52dec166e92eb14cc8d3a
agrutter87 <agrutter87@gmail.com>
2023-05-07 07:30:30 -0500

    Added example source

--- EK_RA6M4/EK_RA6M4_MicroROS_USB/src/hal_entry.c
+++ EK_RA6M4/EK_RA6M4_MicroROS_USB/src/hal_entry.c
@@ -1,35 +1,21 @@
-/***********************************************************************************************************************
- * Copyright [2020-2023] Renesas Electronics Corporation and/or its affiliates.  All Rights Reserved.
- *
- * This software and documentation are supplied by Renesas Electronics America Inc. and may only be used with products
- * of Renesas Electronics Corp. and its affiliates ("Renesas").  No other uses are authorized.  Renesas products are
- * sold pursuant to Renesas terms and conditions of sale.  Purchasers are solely responsible for the selection and use
- * of Renesas products and Renesas assumes no liability.  No license, express or implied, to any intellectual property
- * right is granted by Renesas. This software is protected under all applicable laws, including copyright laws. Renesas
- * reserves the right to change or discontinue this software and/or this documentation. THE SOFTWARE AND DOCUMENTATION
- * IS DELIVERED TO YOU "AS IS," AND RENESAS MAKES NO REPRESENTATIONS OR WARRANTIES, AND TO THE FULLEST EXTENT
- * PERMISSIBLE UNDER APPLICABLE LAW, DISCLAIMS ALL WARRANTIES, WHETHER EXPLICITLY OR IMPLICITLY, INCLUDING WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT, WITH RESPECT TO THE SOFTWARE OR
- * DOCUMENTATION.  RENESAS SHALL HAVE NO LIABILITY ARISING OUT OF ANY SECURITY VULNERABILITY OR BREACH.  TO THE MAXIMUM
- * EXTENT PERMITTED BY LAW, IN NO EVENT WILL RENESAS BE LIABLE TO YOU IN CONNECTION WITH THE SOFTWARE OR DOCUMENTATION
- * (OR ANY PERSON OR ENTITY CLAIMING RIGHTS DERIVED FROM YOU) FOR ANY LOSS, DAMAGES, OR CLAIMS WHATSOEVER, INCLUDING,
- * WITHOUT LIMITATION, ANY DIRECT, CONSEQUENTIAL, SPECIAL, INDIRECT, PUNITIVE, OR INCIDENTAL DAMAGES; ANY LOST PROFITS,
- * OTHER ECONOMIC DAMAGE, PROPERTY DAMAGE, OR PERSONAL INJURY; AND EVEN IF RENESAS HAS BEEN ADVISED OF THE POSSIBILITY
- * OF SUCH LOSS, DAMAGES, CLAIMS OR COSTS.
- **********************************************************************************************************************/
-
 #include "hal_data.h"
+#include "./utils.h"
+#include <microros_transports.h>
+#include <microros_allocators.h>
 
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rcutils/allocator.h>
+#include <rmw_microros/rmw_microros.h>
+
+FSP_CPP_HEADER
 void R_BSP_WarmStart(bsp_warm_start_event_t event);
+FSP_CPP_FOOTER
 
-extern bsp_leds_t g_bsp_leds;
+void microros_app(void);
 
-/*******************************************************************************************************************//**
- * @brief  Blinky example application
- *
- * Blinks all leds at a rate of 1 second using the software delay function provided by the BSP.
- *
- **********************************************************************************************************************/
 void hal_entry (void)
 {
 #if BSP_TZ_SECURE_BUILD
@@ -38,63 +24,28 @@ void hal_entry (void)
     R_BSP_NonSecureEnter();
 #endif
 
-    /* Define the units to be used with the software delay function */
-    const bsp_delay_units_t bsp_delay_units = BSP_DELAY_UNITS_MILLISECONDS;
+    rmw_uros_set_custom_transport(
+          true,
+          (void *) NULL,
+          renesas_e2_transport_open,
+          renesas_e2_transport_close,
+          renesas_e2_transport_write,
+          renesas_e2_transport_read
+        );
 
-    /* Set the blink frequency (must be <= bsp_delay_units */
-    const uint32_t freq_in_hz = 2;
+    rcl_allocator_t custom_allocator = rcutils_get_zero_initialized_allocator();
+    custom_allocator.allocate = microros_allocate;
+    custom_allocator.deallocate = microros_deallocate;
+    custom_allocator.reallocate = microros_reallocate;
+    custom_allocator.zero_allocate =  microros_zero_allocate;
 
-    /* Calculate the delay in terms of bsp_delay_units */
-    const uint32_t delay = bsp_delay_units / freq_in_hz;
-
-    /* LED type structure */
-    bsp_leds_t leds = g_bsp_leds;
-
-    /* If this board has no LEDs then trap here */
-    if (0 == leds.led_count)
-    {
-        while (1)
-        {
-            ;                          // There are no LEDs on this board
-        }
+    if (!rcutils_set_default_allocator(&custom_allocator)) {
+        printf("Error on default allocators (line %d)\n", __LINE__);
     }
 
-    /* Holds level to set for pins */
-    bsp_io_level_t pin_level = BSP_IO_LEVEL_LOW;
+    set_led_status(LED_BLUE, true);
 
-    while (1)
-    {
-        /* Enable access to the PFS registers. If using r_ioport module then register protection is automatically
-         * handled. This code uses BSP IO functions to show how it is used.
-         */
-        R_BSP_PinAccessEnable();
-
-        /* Update all board LEDs */
-        for (uint32_t i = 0; i < leds.led_count; i++)
-        {
-            /* Get pin to toggle */
-            uint32_t pin = leds.p_leds[i];
-
-            /* Write to this pin */
-            R_BSP_PinWrite((bsp_io_port_pin_t) pin, pin_level);
-        }
-
-        /* Protect PFS registers */
-        R_BSP_PinAccessDisable();
-
-        /* Toggle level for next write */
-        if (BSP_IO_LEVEL_LOW == pin_level)
-        {
-            pin_level = BSP_IO_LEVEL_HIGH;
-        }
-        else
-        {
-            pin_level = BSP_IO_LEVEL_LOW;
-        }
-
-        /* Delay */
-        R_BSP_SoftwareDelay(delay, bsp_delay_units);
-    }
+    microros_app();
 }
 
 /*******************************************************************************************************************//**
@@ -122,6 +73,6 @@ void R_BSP_WarmStart (bsp_warm_start_event_t event)
         /* C runtime environment and system clocks are setup. */
 
         /* Configure pins. */
-        R_IOPORT_Open(&g_ioport_ctrl, g_ioport.p_cfg);
+        R_IOPORT_Open(&g_ioport_ctrl, &g_bsp_pin_cfg);
     }
 }
