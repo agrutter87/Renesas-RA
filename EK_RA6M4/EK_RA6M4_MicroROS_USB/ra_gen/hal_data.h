df6ca11d4d8f6d3b4964c13ede3a0235986ee1be
agrutter87 <agrutter87@gmail.com>
2023-05-07 07:07:37 -0500

    Configured USB PCDC, using PIPE1 and PIPE2 for Bulk In and Out

--- EK_RA6M4/EK_RA6M4_MicroROS_USB/ra_gen/hal_data.h
+++ EK_RA6M4/EK_RA6M4_MicroROS_USB/ra_gen/hal_data.h
@@ -4,9 +4,29 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
+#include "r_usb_basic.h"
+#include "r_usb_basic_api.h"
+#include "r_usb_pcdc_api.h"
 #include "r_agt.h"
 #include "r_timer_api.h"
 FSP_HEADER
+/* Basic on USB Instance. */
+extern const usb_instance_t g_basic0;
+
+/** Access the USB instance using these structures when calling API functions directly (::p_api is not used). */
+extern usb_instance_ctrl_t g_basic0_ctrl;
+extern const usb_cfg_t g_basic0_cfg;
+
+#ifndef NULL
+void NULL(void*);
+#endif
+
+#if 2 == BSP_CFG_RTOS
+#ifndef NULL
+void NULL(usb_event_info_t *, usb_hdl_t, usb_onoff_t);
+#endif
+#endif
+/** CDC Driver on USB Instance. */
 /** AGT Timer Instance */
 extern const timer_instance_t g_timer0;
 
--- EK_RA6M4/EK_RA6M4_MicroROS_USB/ra_gen/vector_data.c
+++ EK_RA6M4/EK_RA6M4_MicroROS_USB/ra_gen/vector_data.c
@@ -5,9 +5,17 @@
         BSP_DONT_REMOVE const fsp_vector_t g_vector_table[BSP_ICU_VECTOR_MAX_ENTRIES] BSP_PLACE_IN_SECTION(BSP_SECTION_APPLICATION_VECTORS) =
         {
                         [0] = agt_int_isr, /* AGT0 INT (AGT interrupt) */
+            [1] = usbfs_interrupt_handler, /* USBFS INT (USBFS interrupt) */
+            [2] = usbfs_resume_handler, /* USBFS RESUME (USBFS resume interrupt) */
+            [3] = usbfs_d0fifo_handler, /* USBFS FIFO 0 (DMA transfer request 0) */
+            [4] = usbfs_d1fifo_handler, /* USBFS FIFO 1 (DMA transfer request 1) */
         };
         const bsp_interrupt_event_t g_interrupt_event_link_select[BSP_ICU_VECTOR_MAX_ENTRIES] =
         {
             [0] = BSP_PRV_IELS_ENUM(EVENT_AGT0_INT), /* AGT0 INT (AGT interrupt) */
+            [1] = BSP_PRV_IELS_ENUM(EVENT_USBFS_INT), /* USBFS INT (USBFS interrupt) */
+            [2] = BSP_PRV_IELS_ENUM(EVENT_USBFS_RESUME), /* USBFS RESUME (USBFS resume interrupt) */
+            [3] = BSP_PRV_IELS_ENUM(EVENT_USBFS_FIFO_0), /* USBFS FIFO 0 (DMA transfer request 0) */
+            [4] = BSP_PRV_IELS_ENUM(EVENT_USBFS_FIFO_1), /* USBFS FIFO 1 (DMA transfer request 1) */
         };
         #endif
