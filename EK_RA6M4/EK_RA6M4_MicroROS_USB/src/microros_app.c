33eb18b20a3cbbb02eaa67e817353c9aa4ffe59f
agrutter87 <agrutter87@gmail.com>
2023-05-08 05:52:57 -0500

    Updated to test subscription
    Moved error handling out of RC_CHECK to allow for printing error-specific RTT messages

--- EK_RA6M4/EK_RA6M4_MicroROS_USB/src/microros_app.c
+++ EK_RA6M4/EK_RA6M4_MicroROS_USB/src/microros_app.c
@@ -10,12 +10,23 @@
 #include <SEGGER_RTT.h>
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){bool l = true; while(1){set_led_status(LED_RED, l = !l); sleep_ms(100);}}}
+#define APP_ERR_TRAP() {bool l = true; while(1){set_led_status(LED_RED, l = !l); sleep_ms(100);}};
 
 void microros_app(void);
+void subscription_callback(const void * msgin);
+
+bool g_new_data    = false;
 
 void microros_app(void)
 {
-    rcl_allocator_t allocator = rcl_get_default_allocator();
+    rcl_ret_t               rc          = RCL_RET_OK;
+    rcl_allocator_t         allocator   = rcl_get_default_allocator();
+    rcl_publisher_t         publisher   = { 0 };
+    rcl_subscription_t      subscriber  = { 0 };
+    rclc_executor_t         executor    = rclc_executor_get_zero_initialized_executor();
+
+    std_msgs__msg__Int32    send_msg    = { 0 };
+    std_msgs__msg__Int32    recv_msg    = { 0 };
 
     //create init_options
     rclc_support_t support;
@@ -23,27 +34,108 @@ void microros_app(void)
 
     // create nodes
 	rcl_node_t node;
-	RCCHECK(rclc_node_init_default(&node, "my_renesas_node", "", &support));
-
-	SEGGER_RTT_printf(0, "rclc_node_init_default success!\r\n");
+	rc = rclc_node_init_default(&node,
+	                            "my_renesas_node",
+	                            "",
+	                            &support);
+	if(rc != RCL_RET_OK)
+	{
+	    SEGGER_RTT_printf(0, "Failed microros_app::rclc_node_init_default, rc = %d!\r\n", rc);
+	    APP_ERR_TRAP();
+	}
+	else
+	{
+	    SEGGER_RTT_printf(0, "rclc_node_init_default success!\r\n");
+	}
 
     // create publisher
-    rcl_publisher_t publisher;
-    RCCHECK(rclc_publisher_init_default(
-        &publisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "int_publisher"));
+    rc = rclc_publisher_init_default(&publisher,
+                                     &node,
+                                     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+                                     "int_publisher");
+    if(rc != RCL_RET_OK)
+    {
+        SEGGER_RTT_printf(0, "Failed microros_app::rclc_publisher_init_default, rc = %d!\r\n", rc);
+        APP_ERR_TRAP();
+    }
+    else
+    {
+        SEGGER_RTT_printf(0, "rclc_publisher_init_default success!\r\n");
+    }
 
-    SEGGER_RTT_printf(0, "rclc_publisher_init_default success!\r\n");
+    // create subscriber
+    rc = rclc_subscription_init_default(&subscriber,
+                                        &node,
+                                        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+                                        "int_subscriber");
+    if(rc != RCL_RET_OK)
+    {
+        SEGGER_RTT_printf(0, "Failed microros_app::rclc_subscription_init_default, rc = %d!\r\n", rc);
+        APP_ERR_TRAP();
+    }
+    else
+    {
+        SEGGER_RTT_printf(0, "rclc_subscription_init_default success!\r\n");
+    }
 
-    std_msgs__msg__Int32 msg;
+    rc = rclc_executor_init(&executor,
+                            &support.context,
+                            1,
+                            &allocator);
+    if(rc != RCL_RET_OK)
+    {
+        SEGGER_RTT_printf(0, "Failed microros_app::rclc_executor_init, rc = %d!\r\n", rc);
+        APP_ERR_TRAP();
+    }
+    else
+    {
+        SEGGER_RTT_printf(0, "rclc_executor_init success!\r\n");
+    }
 
-    msg.data = 0;
+    rc = rclc_executor_add_subscription(&executor,
+                                        &subscriber,
+                                        &recv_msg,
+                                        &subscription_callback,
+                                        ON_NEW_DATA);
+    if(rc != RCL_RET_OK)
+    {
+        SEGGER_RTT_printf(0, "Failed microros_app::rclc_executor_add_subscription, rc = %d!\r\n", rc);
+        APP_ERR_TRAP();
+    }
+    else
+    {
+        SEGGER_RTT_printf(0, "rclc_executor_add_subscription success!\r\n");
+    }
 
-    for(;;){
-        rcl_publish(&publisher, &msg, NULL);
-        msg.data++;
+    while(1)
+    {
+        rclc_executor_spin_some(&executor, 1000 * 1000);
+        if(g_new_data)
+        {
+            g_new_data = false;
+            send_msg.data = recv_msg.data;
+        }
+
+        rc = rcl_publish(&publisher, &send_msg, NULL);
+        if(rc != RCL_RET_OK)
+        {
+            SEGGER_RTT_printf(0, "Failed microros_app::rcl_publish, rc = %d!\r\n", rc);
+        }
+
+        send_msg.data++;
+
         sleep_ms(100);
     }
+}
+
+// Implementation example:
+void subscription_callback(const void * msgin)
+{
+    // Cast received message to used type
+    const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+
+    // Process message
+    SEGGER_RTT_printf(0, "Received: %d\n", msg->data);
+
+    g_new_data = true;
 }
