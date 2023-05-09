2aa734fc51a96fa57606d57841db45354e73b840
agrutter87 <agrutter87@gmail.com>
2023-05-08 19:03:09 -0500

    Created initial ThreadX variant of MicroROS project

---
+++ EK_RA6M4/EK_RA6M4_MicroROS_USB_ThreadX/ra_gen/blinky_thread.h
@@ -0,0 +1,14 @@
+/* generated thread header file - do not edit */
+#ifndef BLINKY_THREAD_H_
+#define BLINKY_THREAD_H_
+#include "bsp_api.h"
+#include "tx_api.h"
+#include "hal_data.h"
+#ifdef __cplusplus
+                extern "C" void blinky_thread_entry(void);
+                #else
+extern void blinky_thread_entry(void);
+#endif
+FSP_HEADER
+FSP_FOOTER
+#endif /* BLINKY_THREAD_H_ */
