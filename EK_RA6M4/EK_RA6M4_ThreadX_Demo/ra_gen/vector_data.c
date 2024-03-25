a5ed90935bee7c05ae4c57ea4a7b86bae2503c57
agrutter87 <agrutter87@gmail.com>
2024-03-24 19:13:41 -0500

    Added and configured UART0, 7, and 9

--- EK_RA6M4/EK_RA6M4_ThreadX_Demo/ra_gen/vector_data.c
+++ EK_RA6M4/EK_RA6M4_ThreadX_Demo/ra_gen/vector_data.c
@@ -4,10 +4,34 @@
 #if VECTOR_DATA_IRQ_COUNT > 0
         BSP_DONT_REMOVE const fsp_vector_t g_vector_table[BSP_ICU_VECTOR_MAX_ENTRIES] BSP_PLACE_IN_SECTION(BSP_SECTION_APPLICATION_VECTORS) =
         {
-                    };
+                        [0] = sci_uart_rxi_isr, /* SCI0 RXI (Receive data full) */
+            [1] = sci_uart_txi_isr, /* SCI0 TXI (Transmit data empty) */
+            [2] = sci_uart_tei_isr, /* SCI0 TEI (Transmit end) */
+            [3] = sci_uart_eri_isr, /* SCI0 ERI (Receive error) */
+            [4] = sci_uart_rxi_isr, /* SCI7 RXI (Receive data full) */
+            [5] = sci_uart_txi_isr, /* SCI7 TXI (Transmit data empty) */
+            [6] = sci_uart_tei_isr, /* SCI7 TEI (Transmit end) */
+            [7] = sci_uart_eri_isr, /* SCI7 ERI (Receive error) */
+            [8] = sci_uart_rxi_isr, /* SCI9 RXI (Receive data full) */
+            [9] = sci_uart_txi_isr, /* SCI9 TXI (Transmit data empty) */
+            [10] = sci_uart_tei_isr, /* SCI9 TEI (Transmit end) */
+            [11] = sci_uart_eri_isr, /* SCI9 ERI (Receive error) */
+        };
         #if BSP_FEATURE_ICU_HAS_IELSR
         const bsp_interrupt_event_t g_interrupt_event_link_select[BSP_ICU_VECTOR_MAX_ENTRIES] =
         {
+            [0] = BSP_PRV_VECT_ENUM(EVENT_SCI0_RXI,GROUP0), /* SCI0 RXI (Receive data full) */
+            [1] = BSP_PRV_VECT_ENUM(EVENT_SCI0_TXI,GROUP1), /* SCI0 TXI (Transmit data empty) */
+            [2] = BSP_PRV_VECT_ENUM(EVENT_SCI0_TEI,GROUP2), /* SCI0 TEI (Transmit end) */
+            [3] = BSP_PRV_VECT_ENUM(EVENT_SCI0_ERI,GROUP3), /* SCI0 ERI (Receive error) */
+            [4] = BSP_PRV_VECT_ENUM(EVENT_SCI7_RXI,GROUP4), /* SCI7 RXI (Receive data full) */
+            [5] = BSP_PRV_VECT_ENUM(EVENT_SCI7_TXI,GROUP5), /* SCI7 TXI (Transmit data empty) */
+            [6] = BSP_PRV_VECT_ENUM(EVENT_SCI7_TEI,GROUP6), /* SCI7 TEI (Transmit end) */
+            [7] = BSP_PRV_VECT_ENUM(EVENT_SCI7_ERI,GROUP7), /* SCI7 ERI (Receive error) */
+            [8] = BSP_PRV_VECT_ENUM(EVENT_SCI9_RXI,GROUP0), /* SCI9 RXI (Receive data full) */
+            [9] = BSP_PRV_VECT_ENUM(EVENT_SCI9_TXI,GROUP1), /* SCI9 TXI (Transmit data empty) */
+            [10] = BSP_PRV_VECT_ENUM(EVENT_SCI9_TEI,GROUP2), /* SCI9 TEI (Transmit end) */
+            [11] = BSP_PRV_VECT_ENUM(EVENT_SCI9_ERI,GROUP3), /* SCI9 ERI (Receive error) */
         };
         #endif
         #endif
--- EK_RA6M4/EK_RA6M4_ThreadX_Demo/ra_gen/vector_data.h
+++ EK_RA6M4/EK_RA6M4_ThreadX_Demo/ra_gen/vector_data.h
@@ -6,11 +6,39 @@
         #endif
 /* Number of interrupts allocated */
 #ifndef VECTOR_DATA_IRQ_COUNT
-#define VECTOR_DATA_IRQ_COUNT    (0)
+#define VECTOR_DATA_IRQ_COUNT    (12)
 #endif
 /* ISR prototypes */
+void sci_uart_rxi_isr(void);
+void sci_uart_txi_isr(void);
+void sci_uart_tei_isr(void);
+void sci_uart_eri_isr(void);
 
 /* Vector table allocations */
+#define VECTOR_NUMBER_SCI0_RXI ((IRQn_Type) 0) /* SCI0 RXI (Receive data full) */
+#define SCI0_RXI_IRQn          ((IRQn_Type) 0) /* SCI0 RXI (Receive data full) */
+#define VECTOR_NUMBER_SCI0_TXI ((IRQn_Type) 1) /* SCI0 TXI (Transmit data empty) */
+#define SCI0_TXI_IRQn          ((IRQn_Type) 1) /* SCI0 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI0_TEI ((IRQn_Type) 2) /* SCI0 TEI (Transmit end) */
+#define SCI0_TEI_IRQn          ((IRQn_Type) 2) /* SCI0 TEI (Transmit end) */
+#define VECTOR_NUMBER_SCI0_ERI ((IRQn_Type) 3) /* SCI0 ERI (Receive error) */
+#define SCI0_ERI_IRQn          ((IRQn_Type) 3) /* SCI0 ERI (Receive error) */
+#define VECTOR_NUMBER_SCI7_RXI ((IRQn_Type) 4) /* SCI7 RXI (Receive data full) */
+#define SCI7_RXI_IRQn          ((IRQn_Type) 4) /* SCI7 RXI (Receive data full) */
+#define VECTOR_NUMBER_SCI7_TXI ((IRQn_Type) 5) /* SCI7 TXI (Transmit data empty) */
+#define SCI7_TXI_IRQn          ((IRQn_Type) 5) /* SCI7 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI7_TEI ((IRQn_Type) 6) /* SCI7 TEI (Transmit end) */
+#define SCI7_TEI_IRQn          ((IRQn_Type) 6) /* SCI7 TEI (Transmit end) */
+#define VECTOR_NUMBER_SCI7_ERI ((IRQn_Type) 7) /* SCI7 ERI (Receive error) */
+#define SCI7_ERI_IRQn          ((IRQn_Type) 7) /* SCI7 ERI (Receive error) */
+#define VECTOR_NUMBER_SCI9_RXI ((IRQn_Type) 8) /* SCI9 RXI (Receive data full) */
+#define SCI9_RXI_IRQn          ((IRQn_Type) 8) /* SCI9 RXI (Receive data full) */
+#define VECTOR_NUMBER_SCI9_TXI ((IRQn_Type) 9) /* SCI9 TXI (Transmit data empty) */
+#define SCI9_TXI_IRQn          ((IRQn_Type) 9) /* SCI9 TXI (Transmit data empty) */
+#define VECTOR_NUMBER_SCI9_TEI ((IRQn_Type) 10) /* SCI9 TEI (Transmit end) */
+#define SCI9_TEI_IRQn          ((IRQn_Type) 10) /* SCI9 TEI (Transmit end) */
+#define VECTOR_NUMBER_SCI9_ERI ((IRQn_Type) 11) /* SCI9 ERI (Receive error) */
+#define SCI9_ERI_IRQn          ((IRQn_Type) 11) /* SCI9 ERI (Receive error) */
 #ifdef __cplusplus
         }
         #endif
